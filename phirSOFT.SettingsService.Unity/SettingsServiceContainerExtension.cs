927c10c0d2453edc1dfb131c0fa095ddd3c6236d
Philemon Eichin <phileicht@gmx.de>
2019-09-16 10:47:44 +0200

    Implement SettingsServiceContainer Extension
    This aims to resolve #2. But it requires a quirky solution following
    [this](https://github.com/unitycontainer/abstractions/issues/109) issue
    on the unity repo.
    
    It is likely possible, that this will break in future versions.

---
+++ phirSOFT.SettingsService.Unity/SettingsServiceContainerExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Nito.AsyncEx;
+using phirSOFT.SettingsService.Abstractions;
+using Unity.Builder;
+using Unity.Extension;
+using Unity.Processors;
+using Unity.Resolution;
+
+namespace phirSOFT.SettingsService.Unity
+{
+    public class SettingsServiceContainerExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            var strategies = (IEnumerable<MemberProcessor>)Context.BuildPlanStrategies;
+            var processor = (ConstructorProcessor)strategies.First(s => s is ConstructorProcessor);
+            processor.Add(typeof(SettingValueAttribute), _ => null, SettingResolutionFactory);
+        }
+
+        private ResolveDelegate<BuilderContext> SettingResolutionFactory(Attribute attribute, object info, object value)
+        {
+            var type = ((ParameterInfo)info).ParameterType;
+            return (ref BuilderContext context) =>
+            {
+                if (!(attribute is SettingValueAttribute settingValueAttribute))
+                    return value;
+
+                IReadOnlySettingsService readOnlySettingsService = (IReadOnlySettingsService)
+                    context.Resolve(
+                        typeof(IReadOnlySettingsService),
+                        ((SettingValueAttribute)attribute).ServiceInstance);
+                return AsyncContext.Run(
+                    () => readOnlySettingsService.GetSettingAsync(settingValueAttribute.SettingKey, settingValueAttribute.SettingType ?? type));
+
+            };
+        }
+    }
+}
